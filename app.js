b660400cf9215938c6f93948ee0fe59d1d83b99c
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-01-28 08:17:47 -0600

    finished project, app.js

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    // console.log(question);
+    const btn = question.querySelector('.question-btn')
+    // console.log(btn);
+    btn.addEventListener('click', function(){
+        // question.classList.toggle('show-text')
+
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove('show-text')
+            }
+        })
+
+        question.classList.toggle('show-text')
+
+
+    //     if(question.classList.contains('show-text')){
+    //         question.classList.toggle('show-text')
+    //     }else{
+    //         question.classList.toggle('show-text')
+    //     }
+    })
+
+
+})
+
+
+
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn')
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach(function(btn){
-    btn.addEventListener('click', function(){
-        btn.classList.toggle('show-text')
-    })
-})
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         // my version
+//         // document.querySelector('article').classList.toggle('show-text');
+
+//         // tutorial version
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
